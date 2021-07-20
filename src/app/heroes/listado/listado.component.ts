5e7155d66aabc6fe3fb615d4dc7012d47638886a
Jonathan <jonulisanpar@gmail.com>
2021-07-20 12:02:55 -0500

    Seccion 4: Introduccion a Angular
    Capitulo 44: Crear componente de forma automatica
    Capotulo 45: Directiva *ngFor

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ListadoComponent {
 
-  constructor() {
-    console.log('constructor');
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
+  heroeBorrado: string = "";
+
+  borrarElemento(): void {
+    if (this.heroes.length != 0) {
+      console.log("Borrando.....");
+      this.heroeBorrado = this.heroes.pop() || '';
+    } else {
+      console.log("Lista limpia");
+      this.heroeBorrado = "Sin heroes";
+    }
   }
 
 }
