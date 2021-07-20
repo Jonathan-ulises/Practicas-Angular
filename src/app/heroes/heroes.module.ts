839335ecc5f8975b04df7eb902df25e56d7e82d3
Jonathan <jonulisanpar@gmail.com>
2021-07-20 12:56:45 -0500

    Seccion 4: Introduccion a Angular
    Capitulo 48: Modulos

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { HeroComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+
+@NgModule({
+    //Componentes del modulo
+    declarations: [
+        HeroComponent,
+        ListadoComponent
+    ] ,
+    //Componentes con que deseamos que sean publicos para toda la aplicacion
+    exports: [
+        ListadoComponent
+    ] ,
+    //Importacion de Modulos
+    imports: [
+        CommonModule
+    ]
+})
+export class HeroesModule {
+
+}
