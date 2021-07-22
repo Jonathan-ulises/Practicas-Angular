1ba9a6ec150abec93096df8eb68c817f9998f171
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-07-22 01:05:07 -0500

    Seccion 5: Expandiendo nuestras bases
    Capitulo 58: ngModel
    En este capitulo se vio la forma de obtener los datos de un input y de
    manipular una variable del TS con un formulario; todo esto logrado con
    el Two Way DataBinding.

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Personajes {
+  nombre: string;
+  poder : number;
+}
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  nuevo: Personajes = {
+    nombre: 'Trucks',
+    poder: 14000
   }
 
+  agregar(): void {
+    console.log(this.nuevo);
+  }
+  
+  cambiarNombre( event: any ) {
+    console.log(event.target.value);
+  }
 }
